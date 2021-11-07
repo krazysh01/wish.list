6942ef39b4eb0d804a25ca27c287c1004da80feb
krazysh01 <kylesh01@gmail.com>
2021-11-07 22:26:16 +0200

    Updated Deploy Scripts Added deploy script for windows Changed deploy commands to platform specific commands

---
+++ deploy.ps1
@@ -0,0 +1,13 @@
+# abort on errors
+$ErrorActionPreference = "Stop"
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:krazysh01/wish.list.git master:gh-pages
+cd ..
